2333f1acaf206de7dc68b8c5323a087b252f1531
thu4nvd <thuanvd@outlook.com>
2021-01-04 07:54:01 +0700

    install morgan log console

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
-    var a = 1
-    var b = 2
-    var c = a + b
   res.send('Hello World! ${c}')
 })
 
